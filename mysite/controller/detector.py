0b79ffb562ff9b7f51526572a291ff1889388b95
XuYiFanHHH <yf-xu16@mails.tsinghua.edu.cn>
2018-09-13 10:31:01 +0800

    remove useless files

--- mysite/controller/detector.py
+++ mysite/controller/detector.py
@@ -1,20 +1,9 @@
 # coding=utf-8
 from yolov3.yolo import YOLO
 
-# __all__ = ["cal1"]
 
 yolo = None
 isBegin = True
 
-# class _Calculate(object):
-#     def __init__(self): pass
-
-    # def cal1(self):
-    #     # do some calculate
-    #     return
-
 if yolo is None:
     yolo = YOLO()
-
-# def cal1():
-#     return yolo.cal1()
